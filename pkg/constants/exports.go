b7c396d8abddd32055881a3a88409565a767d777
Eric Hotinger <erhoting@microsoft.com>
2018-02-16 12:24:49 -0800

    Refactor other constants. Add exit_codes constants

--- pkg/constants/exports.go
+++ pkg/constants/exports.go
@@ -1,48 +1,50 @@
 package constants
 
-const acrBuildPrefix = "ACR_BUILD_"
-const dockerDomainPrefix = acrBuildPrefix + "DOCKER_"
-const gitDomainPrefix = acrBuildPrefix + "GIT_"
-const dockerComposeDomainPrefix = acrBuildPrefix + "DOCKER_COMPOSE_"
+const (
+	acrBuildPrefix            = "ACR_BUILD_"
+	dockerDomainPrefix        = acrBuildPrefix + "DOCKER_"
+	gitDomainPrefix           = acrBuildPrefix + "GIT_"
+	dockerComposeDomainPrefix = acrBuildPrefix + "DOCKER_COMPOSE_"
 
-// ExportsWorkingDir is the variable name for checkout dir
-const ExportsWorkingDir = acrBuildPrefix + "WORKING_DIR"
+	// ExportsWorkingDir is the variable name for checkout dir
+	ExportsWorkingDir = acrBuildPrefix + "WORKING_DIR"
 
-// ExportsDockerfilePath is the variable name for docker file path
-const ExportsDockerfilePath = dockerDomainPrefix + "FILE"
+	// ExportsDockerfilePath is the variable name for docker file path
+	ExportsDockerfilePath = dockerDomainPrefix + "FILE"
 
-// ExportsDockerBuildContext is the docker build context directory
-const ExportsDockerBuildContext = dockerDomainPrefix + "CONTEXT"
+	// ExportsDockerBuildContext is the docker build context directory
+	ExportsDockerBuildContext = dockerDomainPrefix + "CONTEXT"
 
-// ExportsDockerPushImage is the image name to push to
-const ExportsDockerPushImage = dockerDomainPrefix + "PUSH_TO"
+	// ExportsDockerPushImage is the image name to push to
+	ExportsDockerPushImage = dockerDomainPrefix + "PUSH_TO"
 
-// ExportsDockerRegistry is the docker registry to push to
-const ExportsDockerRegistry = dockerDomainPrefix + "REGISTRY"
+	// ExportsDockerRegistry is the docker registry to push to
+	ExportsDockerRegistry = dockerDomainPrefix + "REGISTRY"
 
-// ExportsDockerComposeFile is the docker compose file used for build and push
-const ExportsDockerComposeFile = dockerComposeDomainPrefix + "FILE"
+	// ExportsDockerComposeFile is the docker compose file used for build and push
+	ExportsDockerComposeFile = dockerComposeDomainPrefix + "FILE"
 
-// ExportsGitSource is the current git source URL
-const ExportsGitSource = gitDomainPrefix + "SOURCE"
+	// ExportsGitSource is the current git source URL
+	ExportsGitSource = gitDomainPrefix + "SOURCE"
 
-// ExportsGitBranch is current git branch
-const ExportsGitBranch = gitDomainPrefix + "BRANCH"
+	// ExportsGitBranch is current git branch
+	ExportsGitBranch = gitDomainPrefix + "BRANCH"
 
-// ExportsGitHeadRev is the current git head revision
-const ExportsGitHeadRev = gitDomainPrefix + "HEAD_REV"
+	// ExportsGitHeadRev is the current git head revision
+	ExportsGitHeadRev = gitDomainPrefix + "HEAD_REV"
 
-// ExportsGitAuthType is the git authentication type used
-const ExportsGitAuthType = gitDomainPrefix + "AUTH_TYPE"
+	// ExportsGitAuthType is the git authentication type used
+	ExportsGitAuthType = gitDomainPrefix + "AUTH_TYPE"
 
-// ExportsGitUser is the current git user
-const ExportsGitUser = gitDomainPrefix + "USER"
+	// ExportsGitUser is the current git user
+	ExportsGitUser = gitDomainPrefix + "USER"
 
-// ExportsPushOnSuccess is the boolean value denoting whether the build will push on success
-const ExportsPushOnSuccess = acrBuildPrefix + "PUSH_ON_SUCCESS"
+	// ExportsPushOnSuccess is the boolean value denoting whether the build will push on success
+	ExportsPushOnSuccess = acrBuildPrefix + "PUSH_ON_SUCCESS"
 
-// ExportsBuildNumber is the current build number
-const ExportsBuildNumber = acrBuildPrefix + "NUMBER"
+	// ExportsBuildNumber is the current build number
+	ExportsBuildNumber = acrBuildPrefix + "NUMBER"
 
-// ExportsBuildTimestamp is the timestamp when the build started in ISO format
-const ExportsBuildTimestamp = acrBuildPrefix + "TIMESTAMP"
+	// ExportsBuildTimestamp is the timestamp when the build started in ISO format
+	ExportsBuildTimestamp = acrBuildPrefix + "TIMESTAMP"
+)
