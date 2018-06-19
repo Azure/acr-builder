1014a53b250990cc8168606acca7f15cbb4a79d7
Eric Hotinger <erhoting@microsoft.com>
2018-06-19 12:02:21 -0700

    Adds additional validation tests

--- cli/validation_test.go
+++ cli/validation_test.go
@@ -2,7 +2,6 @@ package cli
 
 import "testing"
 
-// TestValidateIsolation_Valid tests valid isolations.
 func TestValidateIsolation_Valid(t *testing.T) {
 	for k := range isolations {
 		if err := validateIsolation(k); err != nil {
@@ -11,7 +10,6 @@ func TestValidateIsolation_Valid(t *testing.T) {
 	}
 }
 
-// TestValidateIsolation_Invalid tests invalid isolations.
 func TestValidateIsolation_Invalid(t *testing.T) {
 	inValidValues := []string{
 		"hyperv_isolation",
@@ -24,5 +22,41 @@ func TestValidateIsolation_Invalid(t *testing.T) {
 		if err := validateIsolation(value); err == nil {
 			t.Errorf("%s should be an invalid isolation, but it's valid", value)
 		}
+	}
+}
+
+func TestValidateRegistryCreds_Valid(t *testing.T) {
+	if err := validateRegistryCreds("", ""); err != nil {
+		t.Errorf("No creds passed, but received err: %v", err)
+	}
+
+	if err := validateRegistryCreds("foo", "bar"); err != nil {
+		t.Errorf("Username/password provided, but returned an err: %v", err)
+	}
+}
+
+func TestValidateRegistryCreds_Invalid(t *testing.T) {
+	if err := validateRegistryCreds("foo", ""); err == nil {
+		t.Error("Expected an error from a missing password")
+	}
+
+	if err := validateRegistryCreds("", "bar"); err == nil {
+		t.Error("Expected an error from a missing username")
+	}
+}
+
+func TestValidatePush_Valid(t *testing.T) {
+	if err := validatePush(false, "", "bar", "qux"); err != nil {
+		t.Errorf("Credentials shouldn't be required unless push is specified. Err: %v", err)
+	}
+
+	if err := validatePush(true, "foo", "bar", "qux"); err != nil {
+		t.Errorf("All creds are provided, but received an error: %v", err)
+	}
+}
+
+func TestValidatePush_Invalid(t *testing.T) {
+	if err := validatePush(true, "", "bar", "qux"); err == nil {
+		t.Error("Invalid creds provided but no error was returned")
 	}
 }
