e93f2aa67cce77f9f8b570ee799650c08a3c45db
Eric Hotinger <ehotinger@gmail.com>
2018-08-02 14:26:03 -0700

    - Only output results of dependency scanning if there was an error (so that we can aggregate them at the end of Build) - Refactor dependency parsing slightly, add some helper methods and a unit test.

--- baseimages/scanner/models/image_dependencies.go
+++ baseimages/scanner/models/image_dependencies.go
@@ -3,6 +3,10 @@
 
 package models
 
+import (
+	"fmt"
+)
+
 // ImageDependencies denotes docker image dependencies
 type ImageDependencies struct {
 	Image     *ImageReference   `json:"image"`
@@ -18,6 +22,30 @@ type ImageReference struct {
 	Tag        string `json:"tag,omitempty"`
 	Digest     string `json:"digest"`
 	Reference  string `json:"reference"`
+}
+
+// ImageReferencesEquals determines if two image references are equal.
+func ImageReferencesEquals(img1 *ImageReference, img2 *ImageReference) bool {
+	if img1 == nil && img2 == nil {
+		return true
+	}
+	if img1 == nil || img2 == nil {
+		return false
+	}
+
+	return img1.Registry == img2.Registry &&
+		img1.Repository == img2.Repository &&
+		img1.Tag == img2.Tag &&
+		img1.Digest == img2.Digest &&
+		img1.Reference == img2.Reference
+}
+
+// String returns a string representation of an ImageReference.
+func (i *ImageReference) String() string {
+	if i == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("Registry: %s\nRepository: %s\nTag: %s\nDigest: %s\nReference: %s\n", i.Registry, i.Repository, i.Tag, i.Digest, i.Reference)
 }
 
 // GitReference defines the reference to git source code
---
+++ builder/context_test.go
@@ -0,0 +1,74 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package builder
+
+import (
+	"testing"
+
+	"github.com/Azure/acr-builder/baseimages/scanner/models"
+)
+
+func TestGetImageDependencies(t *testing.T) {
+	json := `[{"image":{"registry":"registry.hub.docker.com","repository":"library/scanner","tag":"latest","digest":"","reference":"scanner:latest"},` +
+		`"runtime-dependency":{"registry":"registry.hub.docker.com","repository":"library/alpine","tag":"latest","digest":"","reference":"alpine:latest"},` +
+		`"buildtime-dependency":[{"registry":"registry.hub.docker.com","repository":"library/golang","tag":"1.10-alpine","digest":"test","reference":"golang:1.10-alpine"}],` +
+		`"git":{"git-head-revision":"abcdef"}}]`
+
+	deps, err := getImageDependencies(json)
+	if err != nil {
+		t.Fatalf("Unexpected err while parsing image deps: %v", err)
+	}
+	if len(deps) != 1 {
+		t.Fatalf("Expected 1 dependency, got %d", len(deps))
+	}
+	r := deps[0]
+	if r.Image == nil {
+		t.Fatal("Image shouldn't be nil")
+	}
+	if r.Runtime == nil {
+		t.Fatal("Runtime shouldn't be nil")
+	}
+	if len(r.Buildtime) != 1 {
+		t.Fatalf("Expected 1 buildtime dep, got %d", len(r.Buildtime))
+	}
+	if r.Git == nil {
+		t.Fatal("git shouldn't be nil")
+	}
+
+	expectedImage := &models.ImageReference{
+		Registry:   "registry.hub.docker.com",
+		Repository: "library/scanner",
+		Tag:        "latest",
+		Digest:     "",
+		Reference:  "scanner:latest",
+	}
+	expectedRuntimeDep := &models.ImageReference{
+		Registry:   "registry.hub.docker.com",
+		Repository: "library/alpine",
+		Tag:        "latest",
+		Digest:     "",
+		Reference:  "alpine:latest",
+	}
+	expectedBuildDep := &models.ImageReference{
+		Registry:   "registry.hub.docker.com",
+		Repository: "library/golang",
+		Tag:        "1.10-alpine",
+		Digest:     "test",
+		Reference:  "golang:1.10-alpine",
+	}
+	expectedGitHeadRev := "abcdef"
+
+	if !models.ImageReferencesEquals(r.Image, expectedImage) {
+		t.Errorf("Invalid image ref. Expected %s, got %s", expectedImage.String(), r.Image.String())
+	}
+	if !models.ImageReferencesEquals(r.Runtime, expectedRuntimeDep) {
+		t.Errorf("Invalid runtime dep. Expected %s, got %s", expectedRuntimeDep.String(), r.Runtime.String())
+	}
+	if !models.ImageReferencesEquals(r.Buildtime[0], expectedBuildDep) {
+		t.Errorf("Invalid buildtime dep. Expected %s, got %s", expectedBuildDep.String(), r.Buildtime[0].String())
+	}
+	if r.Git.GitHeadRev != expectedGitHeadRev {
+		t.Errorf("Invalid git head rev. Expected %s, got %s", expectedGitHeadRev, r.Git.GitHeadRev)
+	}
+}
