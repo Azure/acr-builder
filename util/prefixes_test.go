a8616f90c4fc4055f8fd3b41c403c5070e6e02a9
Eric Hotinger <ehotinger@gmail.com>
2018-08-10 08:20:45 -0700

    Disable automatic registry prefix for exec (#208)

--- util/prefixes_test.go
+++ util/prefixes_test.go
@@ -14,30 +14,11 @@ func TestPrefixRegistryToImageName(t *testing.T) {
 		{"", "foo:latest", "foo:latest"},
 		{"foo", "someimg", "foo/someimg"},
 		{"foo", "someimg:bar", "foo/someimg:bar"},
+		{"foo", "library/someimg:bar", "library/someimg:bar"},
 	}
 
 	for _, test := range tests {
 		actual := PrefixRegistryToImageName(test.registry, test.img)
-		if actual != test.expected {
-			t.Errorf("expected %s, got %s", test.expected, actual)
-		}
-	}
-}
-
-func TestPrefixTags(t *testing.T) {
-	tests := []struct {
-		registry string
-		cmd      string
-		expected string
-	}{
-		{"foo.azurecr.io", "build -f Dockerfile . -t test:latest --tag bar", "build -f Dockerfile . -t foo.azurecr.io/test:latest --tag foo.azurecr.io/bar"},
-		{"", "build -t bar/foo:latest . --tag bar", "build -t bar/foo:latest . --tag bar"},
-		{"foo.azurecr.io", "build -f Dockerfile . -t foo.azurecr.io/test:latest", "build -f Dockerfile . -t foo.azurecr.io/test:latest"},
-		{"foo.azurecr.io", "build -f Dockerfile -t library/test:latest", "build -f Dockerfile -t library/test:latest"},
-	}
-
-	for _, test := range tests {
-		actual := PrefixTags(test.cmd, test.registry)
 		if actual != test.expected {
 			t.Errorf("expected %s, got %s", test.expected, actual)
 		}
