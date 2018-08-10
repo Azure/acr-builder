5c5c138962a743966876cc5ccbd03420d6d24e19
Eric Hotinger <ehotinger@gmail.com>
2018-08-10 00:15:21 -0700

    Better parsing for --set

---
+++ templating/base_render_options_test.go
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package templating
+
+import "testing"
+
+func TestParseValues_Valid(t *testing.T) {
+	tests := []struct {
+		values   []string
+		expected string
+	}{
+		{
+			[]string{"a=b", "b===ll", "c=12345", "d=ab=", "e=", "f=sadf=234"},
+			`a: b
+b: ==ll
+c: "12345"
+d: ab=
+e: ""
+f: sadf=234
+`,
+		},
+		{
+			[]string{"a=b", "a=c", "a=d"},
+			`a: d
+`,
+		},
+	}
+
+	for _, test := range tests {
+		actual, err := parseValues(test.values)
+		if err != nil {
+			t.Errorf("Failed to parse vals, err: %v", err)
+		}
+		if actual != test.expected {
+			t.Errorf("Failed to parse values, expected '%s' but got '%s'", test.expected, actual)
+		}
+	}
+}
+
+func TestParseValues_Invalid(t *testing.T) {
+	tests := []struct {
+		values []string
+	}{
+		{[]string{"apple", "=k", "====="}},
+	}
+
+	for _, test := range tests {
+		if _, err := parseValues(test.values); err == nil {
+			t.Errorf("Expected an error during parse values, but it was nil")
+		}
+	}
+}
