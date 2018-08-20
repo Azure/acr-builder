04dfc488769578ae2ef403d0e9e2c3e65c20ea70
Eric Hotinger <ehotinger@gmail.com>
2018-08-20 15:19:09 -0700

    Add exponential backoff for login and push retries (#240)

---
+++ util/backoff.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"math"
+	"time"
+)
+
+const (
+	minBackoffDuration = 250 * time.Millisecond
+	maxBackoffDuration = 5 * time.Second
+	base               = 2.0
+)
+
+// GetExponentialBackoff returns a Duration that increases exponentially with
+// the number of attempts.
+func GetExponentialBackoff(attempt int) time.Duration {
+	if attempt <= 0 {
+		return minBackoffDuration
+	}
+	durationf := float64(minBackoffDuration) * math.Pow(base, float64(attempt))
+	if durationf > math.MaxInt64 {
+		return maxBackoffDuration
+	}
+	duration := time.Duration(durationf)
+	if duration < minBackoffDuration {
+		return minBackoffDuration
+	} else if duration > maxBackoffDuration {
+		return maxBackoffDuration
+	}
+	return duration
+}
