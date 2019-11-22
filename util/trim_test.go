174bb13146ac80e73ad83afe456bb904e60de759
Jeanine Burke <jeburke@microsoft.com>
2019-11-22 01:45:34 +0000

    handle quotes for docker build step args

---
+++ util/trim_test.go
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package util
+
+import "testing"
+
+func TestTrimQuotes(t *testing.T) {
+	tests := []struct {
+		s        string
+		expected string
+	}{
+		{`Dockerfile`, `Dockerfile`},
+		{`"Dockerfile"`, `Dockerfile`},
+		{`'Dockerfile'`, `Dockerfile`},
+		{`'Dockerfile"`, `Dockerfile"`},
+		{`'Dockerfile '`, `Dockerfile `},
+		{`'   Dockerfile '`, `   Dockerfile `},
+	}
+
+	for _, test := range tests {
+		if actual := TrimQuotes(test.s); actual != test.expected {
+			t.Errorf("Expected %v but got %v", test.expected, actual)
+		}
+	}
+}
