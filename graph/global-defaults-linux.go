bd10f4e0745b62cf04aa713b02a89cfc66868633
Bin Du <2686301+northtyphoon@users.noreply.github.com>
2020-09-07 22:03:09 -0700

    update command (#567)

--- graph/global-defaults-linux.go
+++ graph/global-defaults-linux.go
@@ -14,7 +14,7 @@ Branch: "{{.Run.Branch}}"
 
 # Default image aliases, can be used without $ directive in cmd
 acr: mcr.microsoft.com/acr/acr-cli:0.2
-az: mcr.microsoft.com/acr/azure-cli:58f2d12
-bash: mcr.microsoft.com/acr/bash:58f2d12
-curl: mcr.microsoft.com/acr/curl:58f2d12
+az: mcr.microsoft.com/acr/azure-cli:b0352a7
+bash: mcr.microsoft.com/acr/bash:b0352a7
+curl: mcr.microsoft.com/acr/curl:b0352a7
 `
