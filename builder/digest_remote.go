d3df6416893d92e5dd731441d87835c532c675f7
Samarth Shah <samarthmshah@gmail.com>
2021-05-31 19:17:55 +0000

    add a comment

--- builder/digest_remote.go
+++ builder/digest_remote.go
@@ -51,12 +51,25 @@ func (d *remoteDigest) PopulateDigest(ctx context.Context, ref *image.Reference)
 	if err != nil {
 		return err
 	}
+	// Note: (bug), if the final image is NOT pushed in this build,
+	// Then the `Resolve` command below will resolve the digest of the LAST pushed image with the same tag (IFF tag is provided)
+	// Then, the unwanted behavior of runtime dependencies found in THIS run being tracked on OLD image occurs. 
+	// For ex., consider following step:
+	// - id: build
+	//   build: -t $Registry/acb:latest -f Dockerfile .
+	//   env: ["DOCKER_BUILDKIT=1"]
+	// 
+	// Since we do not push the final image "acb:latest", `Resolve` will get digest of the "acb:latest" IFF it was ever pushed before. 
+	// Lets assume old digest was sha256:xyz
+	// Then, "$Registry/acb:latest" with digest "sha256:xyz" will start to track dependencies from THIS run.
+	// But, I assume MOST tasks always Push after Build, so the dependencies are tracked on the last Pushed digest, so it won't be an issue in MOST cases.
+	// TODO: We need to skip attaching the digest to the final image if the final image wasn't pushed in this Task run.
 	_, desc, err := resolver.Resolve(ctx, imageRef)
 	if err == nil {
 		ref.Digest = desc.Digest.String()
 		return nil
 	}
-	// If the image is not pushed yet, it will not have any digest.
+	// If the final image is not pushed ever, it will not have any digest.
 	if errdefs.IsNotFound(err) {
 		return nil
 	}
