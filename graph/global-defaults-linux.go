205e25b45080d568d4a9964c30c580627e7dd696
Jun <wju@microsoft.com>
2023-02-02 15:50:31 +0800

    maint: update acr-cli to v0.7

--- graph/global-defaults-linux.go
+++ graph/global-defaults-linux.go
@@ -13,7 +13,7 @@ Commit: "{{.Run.Commit}}"
 Branch: "{{.Run.Branch}}"
 
 # Default image aliases, can be used without $ directive in cmd
-acr: mcr.microsoft.com/acr/acr-cli:0.6
+acr: mcr.microsoft.com/acr/acr-cli:0.7
 az: mcr.microsoft.com/acr/azure-cli:a9325d5
 bash: mcr.microsoft.com/acr/bash:a9325d5
 curl: mcr.microsoft.com/acr/curl:a9325d5
