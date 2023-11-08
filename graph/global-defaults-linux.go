dc9c679567bcca23915b20f9473baed643d91aa6
Bin Du <bindu@microsoft.com>
2023-11-08 13:14:14 -0800

    chore: update system image

--- graph/global-defaults-linux.go
+++ graph/global-defaults-linux.go
@@ -13,8 +13,8 @@ Commit: "{{.Run.Commit}}"
 Branch: "{{.Run.Branch}}"
 
 # Default image aliases, can be used without $ directive in cmd
-acr: mcr.microsoft.com/acr/acr-cli:0.8
-az: mcr.microsoft.com/acr/azure-cli:b689300
-bash: mcr.microsoft.com/acr/bash:b689300
-curl: mcr.microsoft.com/acr/curl:b689300
+acr: mcr.microsoft.com/acr/acr-cli:0.9
+az: mcr.microsoft.com/acr/azure-cli:9d7dc32
+bash: mcr.microsoft.com/acr/bash:9d7dc32
+curl: mcr.microsoft.com/acr/curl:9d7dc32
 `
