f615c39ea1ac2ef4d6c1c1ca6539d08361d747f0
Bin Du <bindu@microsoft.com>
2024-08-14 07:28:33 -0700

    chore: update system images

--- graph/global-defaults-linux.go
+++ graph/global-defaults-linux.go
@@ -13,9 +13,9 @@ Commit: "{{.Run.Commit}}"
 Branch: "{{.Run.Branch}}"
 
 # Default image aliases, can be used without $ directive in cmd
-acr: mcr.microsoft.com/acr/acr-cli:0.11
-az: mcr.microsoft.com/acr/azure-cli:56f0765
-bash: mcr.microsoft.com/acr/bash:56f0765
-curl: mcr.microsoft.com/acr/curl:56f0765
-cssc: mcr.microsoft.com/acr/cssc:56f0765
+acr: mcr.microsoft.com/acr/acr-cli:0.12
+az: mcr.microsoft.com/acr/azure-cli:43d5c87
+bash: mcr.microsoft.com/acr/bash:43d5c87
+curl: mcr.microsoft.com/acr/curl:43d5c87
+cssc: mcr.microsoft.com/acr/cssc:43d5c87
 `
