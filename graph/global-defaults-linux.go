28a65d78ef008ee6d138c14c9ecc1eeaa7737f74
Ruchi Maheshwari <rumahe@microsoft.com>
2025-03-13 13:45:11 -0700

    Update image versions to latest

--- graph/global-defaults-linux.go
+++ graph/global-defaults-linux.go
@@ -13,9 +13,9 @@ Commit: "{{.Run.Commit}}"
 Branch: "{{.Run.Branch}}"
 
 # Default image aliases, can be used without $ directive in cmd
-acr: mcr.microsoft.com/acr/acr-cli:0.14
-az: mcr.microsoft.com/acr/azure-cli:9fb281c
-bash: mcr.microsoft.com/acr/bash:9fb281c
-curl: mcr.microsoft.com/acr/curl:9fb281c
-cssc: mcr.microsoft.com/acr/cssc:9fb281c
+acr: mcr.microsoft.com/acr/acr-cli:0.15
+az: mcr.microsoft.com/acr/azure-cli:cbcf692
+bash: mcr.microsoft.com/acr/bash:cbcf692
+curl: mcr.microsoft.com/acr/curl:cbcf692
+cssc: mcr.microsoft.com/acr/cssc:cbcf692
 `
