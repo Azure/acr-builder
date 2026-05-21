6da3c347ab1d52c794207118383864f76fe2a08f
Bin Du <2686301+northtyphoon@users.noreply.github.com>
2026-05-20 22:49:37 -0700

    chore: update system images (#731)
    * chore: update system images
    
    * update golang to 1.26.3

--- graph/global-defaults-linux.go
+++ graph/global-defaults-linux.go
@@ -14,8 +14,8 @@ Branch: "{{.Run.Branch}}"
 
 # Default image aliases, can be used without $ directive in cmd
 acr: mcr.microsoft.com/acr/acr-cli:0.19
-az: mcr.microsoft.com/acr/azure-cli:d3e982f
-bash: mcr.microsoft.com/acr/bash:d3e982f
-curl: mcr.microsoft.com/acr/curl:d3e982f
-cssc: mcr.microsoft.com/acr/cssc:d3e982f
+az: mcr.microsoft.com/acr/azure-cli:073c481
+bash: mcr.microsoft.com/acr/bash:073c481
+curl: mcr.microsoft.com/acr/curl:073c481
+cssc: mcr.microsoft.com/acr/cssc:073c481
 `
